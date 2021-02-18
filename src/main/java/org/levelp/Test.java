dc2c30399a2476692be1d545f0f91753a72fb3ef
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2021-02-18 20:16:31 +0300

    Change hello message: add exclamations

--- src/main/java/org/levelp/Test.java
+++ src/main/java/org/levelp/Test.java
@@ -2,6 +2,6 @@ package org.levelp;
 
 public class Test {
     public static void main(String[] arguments) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!!!");
     }
 }
